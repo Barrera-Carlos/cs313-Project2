6d7e6952ba6ff342bc24bfb1e6ba3b01537ae93f
carlos <carlosbarreraojeda@gmail.com>
2018-03-24 13:16:49 -0500

    fixing path

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ function addUserToDb(userName,displayName,password,callback){
   		} else {
         var person = result[0].password;
         //res.status(200).json(person);
-  			res.sendFile('CreateAndSerch.html');
+  			res.sendFile('public/CreateAndSerch.html');
   		}
   	});
   }
