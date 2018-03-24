3eefced72118378a69b01e399305dacbc6431047
carlos <carlosbarreraojeda@gmail.com>
2018-03-24 18:40:04 -0500

    work work work work

--- public/displayScript.js
+++ public/displayScript.js
@@ -1,11 +1,13 @@
 function buildRoomForm(){
   $.ajax({
     type:'GET',
-    url: 'http://localhost:5000/ajaxcall',
+    url: '/ajaxcall',
     dataType: 'json'
   })
   .done(function(data){
     console.log('GET respose:', JSON.stringify(data,"",2));
     $('#getResponse').html(JSON.stringify(data, "", 2));
   });
+
+
 }
