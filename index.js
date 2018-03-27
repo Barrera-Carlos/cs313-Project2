a6f097e36b1f3b8c95b66ebde07071985f0f1a98
carlos <carlosbarreraojeda@gmail.com>
2018-03-26 20:10:27 -0500

    bugs o the bugs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(require('morgan')('dev'));
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var expressSession = require('express-session');
+var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 const path = require('path');
 const { Client } = require('pg');
