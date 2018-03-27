40bf69898a2cf8b10a2cd2e20544a9559887a250
carlos <carlosbarreraojeda@gmail.com>
2018-03-26 20:14:43 -0500

    display the object

--- public/displayScript.js
+++ public/displayScript.js
@@ -1,5 +1,5 @@
 function buildRoomForm(){
   $.get("/ajaxcall", function(data, status){
-          alert("Data: " + data + "\nStatus: " + status);
+          alert("Data: " + data[0] + "\nStatus: " + status);
       });
 }
