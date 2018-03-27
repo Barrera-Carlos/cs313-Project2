8a013d3f672cf12784f9f404c2ede38c2cdbec76
carlos <carlosbarreraojeda@gmail.com>
2018-03-26 23:19:32 -0500

    i am

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { Client } = require('pg');
 var port = process.env.PORT || 5000;
 
 //const connectionString = "dbname=d5jgh9e9r7rs3k host=ec2-54-235-146-51.compute-1.amazonaws.com port=5432 user=tukubqgepkcvtn password=a621abf6cdfaaf67344840e60ae648a52ea542d59007c21828b1699c31c61c1b sslmode=require";
-app.use(session({secret:'tati',saveUninitialized: false, resave: false}));
+app.use(session({secret:'tati',saveUninitialized: true, resave: false}));
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.set('views', path.join(__dirname, 'views'));
