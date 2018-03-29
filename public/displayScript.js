ce2d9bf6f2e40d7bda6ed1d7f9202c2f4dfe3056
carlos <carlosbarreraojeda@gmail.com>
2018-03-28 22:10:07 -0500

    yaaas

--- public/displayScript.js
+++ public/displayScript.js
@@ -1,9 +1,13 @@
 function buildRoomForm(){
+  var ul = document.getElementById("list")
+
   $.get("/ajaxcall", function(data, status){
-    $.each(data, function(key,value){
-      $('#list').append($('<li>')).value;
-      console.log(value);
-    })
+    for(var i = 0; i<data.length; i++){
+      var li = document.createElement("LI");
+      var text = document.createTextNode(data[i].room_name );
+      li.appendChild(text);
+      ul.appendChild(li);
+    }
           //alert("Data: " + data[0].room_name + "\nStatus: " + status);
       });
 }
