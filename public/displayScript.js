d100f953eb177f75bafc77a1d849275289685ef9
carlos <carlosbarreraojeda@gmail.com>
2018-03-28 22:02:40 -0500

    dis rooms

--- public/displayScript.js
+++ public/displayScript.js
@@ -2,6 +2,7 @@ function buildRoomForm(){
   $.get("/ajaxcall", function(data, status){
     $.each(data, function(key,value){
       $('#list').append($('<li>')).value;
+      console.log(value);
     })
           //alert("Data: " + data[0].room_name + "\nStatus: " + status);
       });
