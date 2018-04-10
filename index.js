e735ea2bdd34f00416dbb9df328fee14c3341508
carlos <carlosbarreraojeda@gmail.com>
2018-04-09 22:16:05 -0500

    changing some tags

--- index.js
+++ index.js
@@ -239,12 +239,12 @@ function logIn(req,res){
         var cookieId = req.session.id;
         checkForUser(id, function(error, result){
           if(error || result == null){
-              res.status(500).json({success: false, data: "User not found"});
+              res.status(500).json({success: false, data: error});
           }
           else if (result.length == 0 && error == null) {
             queryCookie(id,disName,cookieId, function(error){
               if (error) {
-                res.status(500).json({success: false, data: "User not found"});
+                res.status(500).json({success: false, data: error});
               }
               else {
                 res.sendFile( __dirname + "/public/" +'chooseRoom.html');
